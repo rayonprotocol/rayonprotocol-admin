32e77bb6e2ad546aa41420e7d5ab6943a5f1d852
eunwoo <aufcl4858@naver.com>
2018-10-29 14:56:36 +0900

    Update database info

--- node-server/src/main/controller/configuration.ts
+++ node-server/src/main/controller/configuration.ts
@@ -1,5 +1,6 @@
 export function getDatabaseConfiguration() {
   console.log('process.env', process.env);
+  console.log('process.env', process.env.env);
   console.log({
     host: process.env.RAYON_DB_HOST,
     user: process.env.RAYON_DB_USER,
